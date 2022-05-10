a75ee54c60dfae1ff265ed60282ed89c21dd9d5f
UPSLearn2020 <76274647+UPSLearn@users.noreply.github.com>
2022-05-11 01:34:39 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # Required packages importing
 import os, streamlit as st
 from streamlit_option_menu import option_menu
+from pathlib import Path
 
 st.set_page_config(layout="wide")
 
@@ -10,7 +11,7 @@ hide_streamlit_style = """
             footer {visibility: hidden;}
             </style>
             """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+#st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 filename = './/autogen//files//_configmap.tpl'
 
 def streamlit_menu():
@@ -41,16 +42,29 @@ if selected == "Search":
     msg2 = st.text_area("MSG2")
     
     if st.button('Submit'):
-        # Read in the file
-        with open(filename, 'r') as file :
-            filedata = file.read()
-
-        # Replace the target string
-        filedata = filedata.replace('$msg1', msg1)
-        filedata = filedata.replace('$msg2', msg2)
-
-        # Write the file out again
-        with open(filename, 'w') as file:
-            file.write(filedata)
+        path = ".//autogen//data"
+        # Check whether the specified path exists or not
+        isExist = os.path.exists(path)
+        if not isExist:  
+            # Create a new directory because it does not exist 
+            os.makedirs(path)
+  
+        with open(path + '//msg.txt', 'w') as file:
+            file.write(msg2)
         
-        st.balloons()
+        path = Path(filename)
+
+        if path.is_file():
+            # Read in the file
+            with open(filename, 'r') as file :
+                filedata = file.read()
+            # Replace the target string
+            filedata = filedata.replace('$msg1', msg1)
+            filedata = filedata.replace('$msg2', msg2)
+            # Write the file out again
+            with open(filename, 'w') as file:
+                file.write(filedata)
+            
+        #st.balloons()
+        else:
+            st.write(f'configmap.tpl file does not exist.')
